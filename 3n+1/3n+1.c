10be437f5c6d02f39dc0f51b8c6e8ddbebe37df9
Pedro E. González V <1pedrog@gmail.com>
2015-11-17 22:42:07 -0430

    Code format.

--- 3n+1/3n+1.c
+++ 3n+1/3n+1.c
@@ -4,20 +4,20 @@
 
 unsigned int table[MAX];
 
-int main() {
-    unsigned long n, i, j, tmp, length, max_length;
-    while (scanf("%lu %lu", &i, &j) != EOF) {
-        printf("%lu %lu", i, j);
-        max_length = 0;
-        if (i > j) {
-            tmp = i;
-            i = j;
-            j = tmp;
-        }
+int main(int argc, char **argv) {
+	unsigned long n, i, j, tmp, length, max_length;
+	while (scanf("%lu %lu", &i, &j) != EOF) {
+		printf("%lu %lu", i, j);
+		max_length = 0;
+		if (i > j) {
+			tmp = i;
+			i = j;
+			j = tmp;
+		}
 		while (i <= j) {
-            n = i;
-            length = 1;
-            while (n != 1) {
+			n = i;
+			length = 1;
+			while (n != 1) {
 				if (n < MAX && table[n]) {
 					length += table[n] - 1;
 					n = 1;
@@ -29,14 +29,14 @@ int main() {
 					}
 					length++;
 				}
-            }
+			}
 			table[i] = length;
-            if (length > max_length) {
-                max_length = length;
-            }
-            i++;
-        }
-        printf(" %lu\n", max_length);
-    }
-    return 0;
+			if (length > max_length) {
+				max_length = length;
+			}
+			i++;
+		}
+		printf(" %lu\n", max_length);
+	}
+	return 0;
 }
